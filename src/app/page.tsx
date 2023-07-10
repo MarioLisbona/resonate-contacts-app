23f305e2b0349abbf072f8da81e9e59bfbe4cab9
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-07-10 18:51:35 +1000

    Add template navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,6 +17,39 @@ export default function Home() {
       </Button>
       <Button variant={"whiteSqBtn"}>Test button</Button>
       <Button variant={"linkBtn"}>Test button</Button>
+      <Text textStyle={"heading"}>This is a heading</Text>
+      <Text textStyle={"subheading"}>This is a sub heading</Text>
+      <Text textStyle={"context"}>This is context</Text>
+      <Text textStyle={"context"}>This is context</Text>
+      <Text textStyle={"context"}>This is context</Text>
+      <Button variant={"filledSqLargeBtn"}>Test button</Button>
+      <Button bg={"brandOrange"} variant={"filledSqBtn"}>
+        Test button
+      </Button>
+      <Button variant={"whiteSqBtn"}>Test button</Button>
+      <Button variant={"linkBtn"}>Test button</Button>
+      <Text textStyle={"heading"}>This is a heading</Text>
+      <Text textStyle={"subheading"}>This is a sub heading</Text>
+      <Text textStyle={"context"}>This is context</Text>
+      <Text textStyle={"context"}>This is context</Text>
+      <Text textStyle={"context"}>This is context</Text>
+      <Button variant={"filledSqLargeBtn"}>Test button</Button>
+      <Button bg={"brandOrange"} variant={"filledSqBtn"}>
+        Test button
+      </Button>
+      <Button variant={"whiteSqBtn"}>Test button</Button>
+      <Button variant={"linkBtn"}>Test button</Button>
+      <Text textStyle={"heading"}>This is a heading</Text>
+      <Text textStyle={"subheading"}>This is a sub heading</Text>
+      <Text textStyle={"context"}>This is context</Text>
+      <Text textStyle={"context"}>This is context</Text>
+      <Text textStyle={"context"}>This is context</Text>
+      <Button variant={"filledSqLargeBtn"}>Test button</Button>
+      <Button bg={"brandOrange"} variant={"filledSqBtn"}>
+        Test button
+      </Button>
+      <Button variant={"whiteSqBtn"}>Test button</Button>
+      <Button variant={"linkBtn"}>Test button</Button>
     </Flex>
   );
 }
