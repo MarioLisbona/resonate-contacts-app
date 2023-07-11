193a09a58ea289b96d2133afccc12e9e294aa9c6
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-07-11 14:29:01 +1000

    dynamic metadata for each contacts info page

--- src/app/contacts/[slug]/page.tsx
+++ src/app/contacts/[slug]/page.tsx
@@ -1,16 +1,27 @@
 import React from "react";
 import SectionContainer from "../../components/SectionContainer";
-import { Text } from "@chakra-ui/react";
-import { testContacts } from "@/data/contacts";
 import Header from "@/app/components/Header";
 import { fetchContact } from "@/app/utils";
 import ContactInfo from "./components/ContactInfo";
+import { Metadata } from "next";
 
 type Params = {
   params: {
     slug: string;
   };
 };
+
+export async function generateMetadata({
+  params: { slug },
+}: Params): Promise<Metadata> {
+  const contact = await fetchContact(slug);
+
+  console.log(contact.name);
+
+  return {
+    title: `Contact | ${contact.name} `,
+  };
+}
 
 export default async function Contact({ params: { slug } }: Params) {
   const contact = await fetchContact(slug);
